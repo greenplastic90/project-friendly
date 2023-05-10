fcc2fcbd37d4ae04dfd3f5de26987d003dd18dc2
greenplastic90 <82140450+greenplastic90@users.noreply.github.com>
2023-05-10 12:14:03 +0300

    added deleteIcons to userIcons controller and route

--- controllers/userIcon.js
+++ controllers/userIcon.js
@@ -17,3 +17,27 @@ export const getAllUserIcons = async (_req, res) => {
 		return res.status(422).json(err)
 	}
 }
+
+export const deleteIcons = async (req, res) => {
+	try {
+		const { ids } = req.query
+
+		if (!ids) {
+			return res.status(400).json({ message: 'Please provide ids as a query parameter.' })
+		}
+
+		// Split the comma-separated list of ids and create an array.
+		const idArray = ids.split(',')
+
+		// Using deleteMany with $in operator to delete all matching documents.
+		const result = await UserIcon.deleteMany({ _id: { $in: idArray } })
+
+		if (result.n === 0) {
+			return res.status(404).json({ message: 'No icons were found with the provided ids.' })
+		}
+
+		return res.status(200).json({ message: `${result.deletedCount} icon(s) successfully deleted.` })
+	} catch (err) {
+		return res.status(422).json(err)
+	}
+}
